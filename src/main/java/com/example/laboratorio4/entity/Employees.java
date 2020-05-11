651c14c7dc76b22b29a8de594130e82ceafaa5b9
Royer <r.yangali@pucp.pe>
2020-05-11 18:55:59 -0500

    entidad employees royer

--- src/main/java/com/example/laboratorio4/entity/Employees.java
+++ src/main/java/com/example/laboratorio4/entity/Employees.java
@@ -1,11 +1,135 @@
-package com.example.laboratorio4.entity;
+package com.example.laboratorio3.entity;
+
+import com.example.laboratorio4.entity.Departments;
+import com.example.laboratorio4.entity.Jobs;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-
+import java.util.Date;
 
 @Entity
 @Table(name="employees")
 public class Employees {
 
-    //COMPLETAR
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private int employeeid;
+    @Column(name = "first_name")
+    private String firstname;
+    @Column(name = "last_name",nullable = false)
+    private String lastname;
+    @Column(nullable = false)
+    private String email;
+    private String password;
+    @Column(name = "phone_number")
+    private String phonenumber;
+    @Column(name = "hire_date")
+    private Date hiredate;
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Jobs jobid;
+    private int salary;
+    @Column(name = "manager_id")
+    private Integer managerid;
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Departments departmentid;
+    private int enabled;
+
+    public int getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(int employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Jobs getJobid() {
+        return jobid;
+    }
+
+    public void setJobid(Jobs jobid) {
+        this.jobid = jobid;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getManagerid() {
+        return managerid;
+    }
+
+    public void setManagerid(int managerid) {
+        this.managerid = managerid;
+    }
+
+    public Departments getDepartmentid() {
+        return departmentid;
+    }
+
+    public void setDepartmentid(Departments departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
 }
