9c03d275949dba8c6d796c62b7adfcb43780ae39
Royer <r.yangali@pucp.pe>
2020-05-11 20:33:51 -0500

    autowiredddd

--- src/main/java/com/example/laboratorio4/controller/SearchController.java
+++ src/main/java/com/example/laboratorio4/controller/SearchController.java
@@ -1,7 +1,7 @@
 package com.example.laboratorio4.controller;
 
 
-import com.example.laboratorio4.repository.EmployeesRepository;
+import com.example.laboratorio4.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class SearchController {
     @Autowired
     EmployeesRepository employeesRepository;
+
+    @Autowired
+    DepartmentsRepository departmentsRepository;
+
+    @Autowired
+    HistoryRepository historyRepository;
+
+    @Autowired
+    JobsRepository jobsRepository;
+
+    @Autowired
+    LocationsRepository locationsRepository;
 
     @GetMapping(value = {"", "/"})
     public String indice() {
