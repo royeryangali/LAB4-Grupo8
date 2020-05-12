a6a0b15e13be731b4c7a400923c9d8b40b6ed1b4
Royer <r.yangali@pucp.pe>
2020-05-11 20:06:43 -0500

    asdddd

--- src/main/java/com/example/laboratorio4/dto/EmpleadoPorSearch.java
+++ src/main/java/com/example/laboratorio4/dto/EmpleadoPorSearch.java
@@ -1,14 +1,21 @@
 package com.example.laboratorio4.dto;
 
 public interface EmpleadoPorSearch {
-String getNombre();
-String getApellido();
-String getCorreo();
-String getContrasena();
-String getCargo();
-int getSalario();
-String getJefe();
-String getDepartamento();
+    String getNombre();
+
+    String getApellido();
+
+    String getCorreo();
+
+    String getContrasena();
+
+    String getCargo();
+
+    int getSalario();
+
+    String getJefe();
+
+    String getDepartamento();
 }
 
 
