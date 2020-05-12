fd68094c4a6bb1097920993a5d2d3d7000d2abbf
Sarita <atanacio.sarita@pucp.pe>
2020-05-11 19:48:47 -0500

    commit RecursosHumanos

---
+++ src/main/java/com/example/laboratorio4/dto/RecursosHumanos.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio4.dto;
+
+public interface RecursosHumanos {
+
+    String getnombre();
+    String getapellido();
+    String getpuesto();
+    String getfechainicio();
+    String getfechafin();
+    int aniosTrabajados();
+    int mesesTrabajados();
+
+}
